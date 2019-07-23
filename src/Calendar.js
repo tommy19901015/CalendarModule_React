23ae1d107f2c4fc4ff87ed9bea8b3e57aed2f714
tommy <tommy19901015@gmail.com>
2019-07-23 19:31:06 +0800

    [modify] mobile style

--- src/Calendar.js
+++ src/Calendar.js
@@ -6,30 +6,43 @@ import axios from "axios";
 class Calendar extends Component {
     state = {
         // initDate : Moment().format("YYYY/MM/DD"),
-        initDate : Moment().format("2018/10/06"),
+        initDate : Moment().format("2018/12/06"),
         weekText : [{text:"星期日",en:"Sunday"},{text:"星期一",en:"Monday"},{text:"星期二",en:"Tuesday"},{text:"星期三",en:"Wednesday"},{text:"星期四",en:"Thursday"},{text:"星期五",en:"Friday"},{text:"星期六",en:"Saturday"}],
         weekToatalDay : 7,
         tableGrid : 35,
-        jsonUrl : '/json/data1.json',
-        jsonData : []
+        jsonUrl : '/json/data2.json',
+        jsonData : [],
+        isSelectList : true
     }
     componentDidMount = () => {
         this.getJsonData()
-        .then(res => {this.setState({jsonData:res})})  
-        .then(this.setJsonData)    
+        .then(this.sortJsonData)      
     }
     getJsonData = () => {
         return axios.get(this.state.jsonUrl)
         .then( res => res.data.sort((a, b) => Moment(b.date).diff(Moment(a.date))))
         .catch((error) => {console.log(error)});
     }
-    setJsonData = () => {
-        //get now date
-        //set YYYY/MM data 
-        // console.log(this.state.jsonData)
+    sortJsonData = (res) => {
+        const newRes = res.map(o=>{
+            if(Object.keys(o).includes('certain')){
+                o["availableVancancy"] = o["onsell"];
+                delete o["onsell"];
+                o["guaranteed"] = o["certain"];
+                delete o["certain"];
+                o["totalVacnacy"] = o["total"];
+                delete o["total"];
+                o["status"] = o["state"];
+                delete o["state"];
+                return o
+            }
+            return o    
+        })
+        this.setState({jsonData:newRes})
+        return newRes
     }
     renderTitle = () => {
-        const now = Moment(this.state.initDate).format("YYYY MM月"); 
+        const now = Moment(this.state.initDate).format("YYYY MM月");
         const pre = Moment(this.state.initDate).subtract(1, 'months').format("YYYY MM月");
         const next = Moment(this.state.initDate).add(1, 'months').format("YYYY MM月");
         const next2 = Moment(this.state.initDate).add(2, 'months').format("YYYY MM月");
@@ -41,17 +54,24 @@ class Calendar extends Component {
         return(
             <div className="changeMonthBlock">
                 <div className="pre">{'◀'}</div>
-                <div onClick={this.ckickMothTitle} data-info={nowDataInfo + '/01'} className="monthBlock active">{now}</div>
-                <div onClick={this.ckickMothTitle} data-info={nextDataInfo + '/01'} className="monthBlock">{next}</div>
-                <div onClick={this.ckickMothTitle} data-info={next2DataInfo + '/01'} className="monthBlock">{next2}</div>
+                <div onClick={this.ckickMothTitle} data-info={preDataInfo + '/01'} className={this.activeTitle(preDataInfo + '/01')}>{pre}</div>
+                <div onClick={this.ckickMothTitle} data-info={nowDataInfo + '/01'} className={this.activeTitle(nowDataInfo + '/01')}>{now}</div>
+                <div onClick={this.ckickMothTitle} data-info={next2DataInfo + '/01'} className={this.activeTitle(nextDataInfo + '/01')}>{next}</div>
                 <div className="next">{'▶'}</div>
             </div> 
         )
     }
     ckickMothTitle = e => {
+        console.log(e.currentTarget.dataset.info.split('/',2).join(''))
+        this.state.jsonData.map(o=>{
+            // console.log(o.date.split('/',2).join(''))
+        })
+
         this.setState({initDate:e.currentTarget.dataset.info})
-        // console.log(e.currentTarget.dataset.info.split('/',2).join(''))
-        // console.log(this.state.initDate.split('/',2).join(''))
+    }
+    activeTitle = (title) => {
+        return this.state.initDate.split('/',2).join('') === title.split('/',2).join('') ?
+        'monthBlock active' : 'monthBlock'
     }
     renderDay = () => {
         const { initDate,weekText,weekToatalDay,tableGrid } = this.state;
@@ -69,7 +89,9 @@ class Calendar extends Component {
                     year:Moment(initDate).format('YYYY'),
                     month:Moment(initDate).format('MM'),
                     day:d >= endDay ? d : d++,
-                    week:Moment(Moment(initDate).format('YYYY') + "/" + Moment(initDate).format('MM') + "/" + d).format("dddd"),
+                    week_en:Moment(Moment(initDate).format('YYYY') + "/" + Moment(initDate).format('MM') + "/" + d).subtract(1, 'days').format("dddd"),
+                    week_ch : this.state.weekText.filter(o=>(o.en === Moment(Moment(initDate)
+                        .format('YYYY') + "/" + Moment(initDate).format('MM') + "/" + d).subtract(1, 'days').format("dddd") ? o.text : ''))[0].text,
                     empty:false
                 })
             }
@@ -78,37 +100,70 @@ class Calendar extends Component {
                 weekDataArr = [];
             }
         }
-        return monthDataArr.map((obj,i) => <div key={i} className="div_tr">{obj.map((day,j)=>
-                <div key={j} className={day.empty ? 'div_td noDay' : 'div_td'}>
-                    <div className="dayText">{day.empty ? '' : day.day}</div>
-                    <div className="content">{this.renderContent(day)}</div>
+        return monthDataArr.map((obj,i) => <div key={i} className="div_tr">{obj.map((days,j)=>
+                <div key={j} className={days.empty ? 'div_td noDay noGroup' : this.renderContent(days) ? 'div_td hasData' : 'div_td noGroup'}>
+                    <div className="right_block">
+                        <div className="dayText">{days.empty ? '' : days.day}</div>
+                        <div className="week_ch">{days.week_ch}</div>
+                    </div>
+                    <div className="content">{this.renderContent(days)}</div>
                 </div>)}</div>)
     }
     renderContent = (days) => {
         if(!days.empty && this.state.jsonData.length != 0){
            let date = days.year + "/" + days.month + "/" + ((days.day) < 10 ? '0' + days.day : days.day);
            let info = this.state.jsonData.filter(obj => obj.date === date);
-           console.log(info)
            if(info.length != 0){
             return ([
-                // <div>{info[0].date}</div>,
-                <div>{info[0].status}</div>,
-                <div>可賣 : {info[0].availableVancancy}</div>,
-                <div>團位 : {info[0].totalVacnacy}</div>,
-                <div>${info[0].price}</div>
+                <div className={info[0].guaranteed ? 'guaranteed' : 'guaranteed no'} key={'guaranteed'}>成團</div>,
+                <div className={this.statusStyle(info[0].status)} key={'status'}>{info[0].status}</div>,
+                <div className="availableVancancy" key={'availableVancancy'}>可賣 : {info[0].availableVancancy}</div>,
+                <div className="totalVacnacy" key={'totalVacnacy'}>團位 : {info[0].totalVacnacy}</div>,
+                <div className="price" key={'price'}>${this.moneyFormat(info[0].price)}</div>
             ])
            }
+           return false
         }
+    }
+    statusStyle = (status) => {
+        const style = 'status ';
+        switch(status){
+            case '報名' :
+                return style + 'o'
+            case '請洽專員' :
+            case '後補' :
+                return style + 'g'
+            case '關團' :
+            case '額滿' :
+                return style    
+            default :
+                return style 
+        }
+    }
+    moneyFormat = (money) => {
+        if(money && money!=null){
+            money = String(money);
+            let left=money.split('.')[0],right=money.split('.')[1];
+            right = right ? (right.length>=2 ? '.'+right.substr(0,2) : '.'+right+'0') : '';
+            let temp = left.split('').reverse().join('').match(/(\d{1,3})/g);
+            return (Number(money)<0?"-":"") + temp.join(',').split('').reverse().join('')+right;
+        }
+    }
+    changeSelectList = () => {
+        this.setState({isSelectList : !this.state.isSelectList})
     }
     render() {
         return (
-            <div className="main_container">
-                {this.renderTitle()}
-                <div className="div_table">
-                    <div className="div_tr weekBlock">
-                        {this.state.weekText.map((obj,i) => <div className="div_td weekText" key={i}>{obj.text}</div>)}
+            <div>
+                <div className="changeBtn" onClick={this.changeSelectList}>{this.state.isSelectList ? "切換列表顯示" : "切換日歷顯示"}</div>
+                <div className="main_container">
+                    {this.renderTitle()}
+                    <div className={this.state.isSelectList ? "div_table" : "div_table selectList"}>
+                        <div className="div_tr weekBlock">
+                            {this.state.weekText.map((obj,i) => <div className="div_td weekText" key={i}>{obj.text}</div>)}
+                        </div>
+                        {this.renderDay()}
                     </div>
-                    {this.renderDay()}
                 </div>
             </div>
         );
