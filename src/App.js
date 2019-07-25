6292db4d46dc3a70fe7d193734639eff5032642e
tommy <tommy19901015@gmail.com>
2019-07-25 15:27:54 +0800

    [modify] verification input data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import Calendar from './Calendar'
 
 class App extends Component {
     render() {
+        let jsonData =
+        [{
+            "certain": false,
+            "date": "2019/08/15",
+            "price": 62083,
+            "onsell": 46,
+            "total": 337,
+            "state": "預定"
+        },
+        {
+            "certain": false,
+            "date": "2019/10/15",
+            "price": 909,
+            "onsell": 87,
+            "total": 497,
+            "state": "報名"
+        },]
         return (
             <div className="App">
                 {/* <Calendar /> */}
-                <Calendar date={'2018/12/25'}/>    
+                <Calendar jsonData={'/json/data1.json'} />
+                {/* <Calendar date={'2020/5/25'} jsonData={jsonData} /> */}
+                {/* <Calendar date={'2020/5/25'} jsonData={{}} />   */}
             </div>
         );
     }
--- src/Calendar.js
+++ src/Calendar.js
@@ -7,104 +7,109 @@ import propTypes from 'prop-types';
 class Calendar extends Component {
     state = {
         initDate : Moment().format("YYYY/MM/DD"),
-        // initDate : Moment().format("2018/12/06"),
         weekText : [{text:"星期日",en:"Sunday"},{text:"星期一",en:"Monday"},{text:"星期二",en:"Tuesday"},{text:"星期三",en:"Wednesday"},{text:"星期四",en:"Thursday"},{text:"星期五",en:"Friday"},{text:"星期六",en:"Saturday"}],
         weekToatalDay : 7,
-        tableGrid : 35,
-        jsonUrl : '/json/data2.json',
+        tableGrid : 42,
+        jsonUrl : '',
         jsonData : [],
-        isSelectList : true
+        isSelectList : true,
+        inDateRange:true,
+        isErrorUrl:false
     }
-    componentDidMount = () => {
+    componentDidMount = () => {        
         this.getJsonData()
         .then(this.sortJsonData)
-        .then(this.getRecentJsonData)   
+        .then(this.getRecentJsonData)
     }
     getJsonData = () => {
-        return axios.get(this.state.jsonUrl)
-        .then( res => res.data.sort((a, b) => Moment(b.date).diff(Moment(a.date))))
-        .catch((error) => {console.log(error)});
+        let data = this.props.jsonData
+        if(typeof data === 'string'){
+            this.setState({jsonUrl:data})
+            return axios.get(data)
+            .then( res => res.data.sort((a, b) => Moment(b.date).diff(Moment(a.date))))
+            .catch((error) => {this.setState({isErrorUrl:true})});
+        }
+        if(Array.isArray(data)){
+            this.setState({jsonData:data})
+            return Promise.resolve(data)
+                .then( res => res.sort((a, b) => Moment(b.date).diff(Moment(a.date))))
+        }
+        this.setState({isErrorUrl:true});
+        return Promise.resolve('')
     }
     sortJsonData = (res) => {
-        const newRes = res.map(o=>{
-            if(Object.keys(o).includes('certain')){
-                o["availableVancancy"] = o["onsell"];
-                delete o["onsell"];
-                o["guaranteed"] = o["certain"];
-                delete o["certain"];
-                o["totalVacnacy"] = o["total"];
-                delete o["total"];
-                o["status"] = o["state"];
-                delete o["state"];
-            }
-            return o    
-        })
+        let newRes = [];
+        if(typeof res !== 'undefined' && res !== ''){
+            newRes = res.map(o=>{
+                if(Object.keys(o).includes('certain')){
+                    o["availableVancancy"] = o["onsell"];
+                    delete o["onsell"];
+                    o["guaranteed"] = o["certain"];
+                    delete o["certain"];
+                    o["totalVacnacy"] = o["total"];
+                    delete o["total"];
+                    o["status"] = o["state"];
+                    delete o["state"];
+                }
+                return o    
+            })
+        }
         this.setState({jsonData:newRes})
         return newRes
     }
     getRecentJsonData = () => {
-        // moment("not a real date").isValid(); // false
-        const inputDate = (typeof(this.props.date) != "undefined") ? this.props.date : Moment().format("YYYY/MM/DD");
-        const recentDay = this.state.jsonData.map(o=>Math.abs(Moment(inputDate).diff(o.date, 'days')));
-        const min = Math.min(...recentDay);
-        const idxArr = recentDay.map((o,i)=>o===min?i:'').filter(k=>k!=='');
-        let max = 0;
-        let mode = null;
-        let mostRepeated = idxArr.map(i=>this.state.jsonData[i]).reduce((acc, curr) => { 
-            (curr.date in acc) ? acc[curr.date]++ : acc[curr.date] = 1
-            if (max < acc[curr.date]) {
-                max = acc[curr.date];
-                mode = curr.date;
-            }
-            return acc;
-        }, {});
-        let mostRepeatedDate = Object.keys(mostRepeated).reduce((a, b) => mostRepeated[a] > mostRepeated[b] ? a : b);
-        // console.log(mostRepeatedDate)
-        console.log(recentDay)
-
-        this.setState({initDate: this.state.jsonData[idxArr[0]].date});
-        
-
-
-
-        //2.吃資料輸入 -> if手動輸入資料 null -> 比對json資料取最近一筆
-
-        // }
-        //header 的 click
-        //當月份後都沒資料時給尚無資料
-        //data render完顯示第一筆css
-        //props 可以吃array或url
-        
-        //有指定日期 > 沒有資料 > 找相近的月份當資料   如果月數前後天數一樣找資料多的
-        //無指定日期 > 沒有資料 > 拿最近的月份當資料
-
+        if(!this.state.isErrorUrl){
+            const inputDate = (typeof(this.props.date) != "undefined") ? this.props.date : Moment().format("YYYY/MM/DD");
+            const recentDay = this.state.jsonData.map(o=>Math.abs(Moment(inputDate).diff(o.date, 'days')));
+            const min = Math.min(...recentDay);
+            const idxArr = recentDay.map((o,i)=>o===min?i:'').filter(k=>k!=='');
+            let max = 0;
+            let mode = null;
+            let mostRepeated = idxArr.map(i=>this.state.jsonData[i]).reduce((acc, curr) => { 
+                (curr.date in acc) ? acc[curr.date]++ : acc[curr.date] = 1
+                if (max < acc[curr.date]) {
+                    max = acc[curr.date];
+                    mode = curr.date;
+                }
+                return acc;
+            }, {});
+            let mostRepeatedDate = Object.keys(mostRepeated).reduce((a, b) => mostRepeated[a] >= mostRepeated[b] ? a : b);
+            let mostRepeatedDateIdx = idxArr.filter(i=> this.state.jsonData[i].date === mostRepeatedDate ? i : '')[0];    
+            (typeof(mostRepeatedDateIdx) === 'undefined') ? mostRepeatedDateIdx = 0 : mostRepeatedDateIdx = mostRepeatedDateIdx;
+            this.setState({initDate: this.state.jsonData[mostRepeatedDateIdx].date});
+        }else{
+            console.log('error url')
+        }
     }
     renderTitle = () => {
         const now = Moment(this.state.initDate).format("YYYY MM月");
         const pre = Moment(this.state.initDate).subtract(1, 'months').format("YYYY MM月");
         const next = Moment(this.state.initDate).add(1, 'months').format("YYYY MM月");
-        const next2 = Moment(this.state.initDate).add(2, 'months').format("YYYY MM月");
         const nowDataInfo = Moment(this.state.initDate).format("YYYY/MM");
         const preDataInfo = Moment(this.state.initDate).subtract(1, 'months').format("YYYY/MM");
         const nextDataInfo = Moment(this.state.initDate).add(1, 'months').format("YYYY/MM");
-        const next2DataInfo = Moment(this.state.initDate).add(2, 'months').format("YYYY/MM");
-
         return(
             <div className="changeMonthBlock">
+                <div onClick={this.ckickMothTitle} data-info={preDataInfo + '/01'} className={this.activeTitle(preDataInfo + '/01')}>
                 <div className="pre">{'◀'}</div>
-                <div onClick={this.ckickMothTitle} data-info={preDataInfo + '/01'} className={this.activeTitle(preDataInfo + '/01')}>{pre}</div>
+                {pre}</div>
                 <div onClick={this.ckickMothTitle} data-info={nowDataInfo + '/01'} className={this.activeTitle(nowDataInfo + '/01')}>{now}</div>
-                <div onClick={this.ckickMothTitle} data-info={nextDataInfo + '/01'} className={this.activeTitle(nextDataInfo + '/01')}>{next}</div>
+                <div onClick={this.ckickMothTitle} data-info={nextDataInfo + '/01'} className={this.activeTitle(nextDataInfo + '/01')}>{next}
                 <div className="next">{'▶'}</div>
+                </div>
             </div> 
         )
     }
     ckickMothTitle = e => {
-        // console.log(e.currentTarget.dataset.info.split('/',2).join(''))
-        // this.state.jsonData.map(o=>
-        //     o.date.split('/',2).join('') === e.currentTarget.dataset.info.split('/',2).join('')).length === 0 ?
-        //     '' : this.setState({initDate:e.currentTarget.dataset.info})
-        this.setState({initDate:e.currentTarget.dataset.info})
+        if(!this.state.isErrorUrl){
+            let endDay = this.state.jsonData[0].date;
+            let startDay = this.state.jsonData.slice(-1)[0].date;
+            let clickDay = e.currentTarget.dataset.info;
+            this.setState({
+                initDate:clickDay,
+                inDateRange:Moment(clickDay).isBetween(startDay, endDay)
+            })
+        }
     }
     activeTitle = (title) => {
         return this.state.initDate.split('/',2).join('') === title.split('/',2).join('') ?
@@ -196,6 +201,7 @@ class Calendar extends Component {
                 <div className="main_container">
                     {this.renderTitle()}
                     <div className={this.state.isSelectList ? "div_table" : "div_table selectList"}>
+                        {this.state.inDateRange ? '' : <div className="noTravel">本月無出發行程</div>}
                         <div className="div_tr weekBlock">
                             {this.state.weekText.map((obj,i) => <div className="div_td weekText" key={i}>{obj.text}</div>)}
                         </div>
